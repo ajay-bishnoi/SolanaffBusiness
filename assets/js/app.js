15f28af91c2e45bf07e16b2e2710a892df5764a1
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-01-19 15:11:21 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function openNav() {
+  document.querySelector(".closeNav").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross");
+}
